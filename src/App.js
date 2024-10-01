69e0265ae32c5b2b14f9fb61bf2985ecf809f98c
Elizaveta Guseva <liza.guseva@hey.com>
2024-10-01 11:30:28 +0200

    something is funny

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
       <Navbar language={language} setLanguage={setLanguage} />
         <Routes>
           <Route path='/' element={<Home language={language} />} />
-          <Route path='/ArtesaniaLanding' element={<Home language={language} />} />
           <Route path='/demoinfo' element={<InfoDemo language={language} />} />
           <Route path='/demo' element={<Demo language={language} />} />
           <Route path='/dashboard' element={<Dashboard />} >
